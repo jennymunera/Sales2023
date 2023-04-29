d7fa90d4220d8ed666f1560c2942702ced11ae1b
Jenny Munera Serna <jennymunera94@gmail.com>
2023-04-29 00:04:59 -0500

    migration and update database and stablish relations between tables

--- Sales.Shared/Entities/City.cs
+++ Sales.Shared/Entities/City.cs
@@ -1,18 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Sales.Shared.Entities
 {
-    public class Country
+    public class City
     {
         public int Id { get; set; }
 
-        [Display(Name = "Pais")]
+        [Display(Name = "Ciudad")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         [MaxLength(100, ErrorMessage = "El campo {0}  no debe tener más de  {1} caractéres")]
         public string Name { get; set; } = null!;
 
-        public ICollection<State>? States { get; set;}
-
-        public int StatesNumber => States == null ? 0 : States.Count;  
+        public State? State { get; set; }
     }
 }
---
+++ Sales.Shared/Entities/State.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Sales.Shared.Entities
+{
+    public class State
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Estado/Departamento/Provincia")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El campo {0}  no debe tener más de  {1} caractéres")]
+        public string Name { get; set; } = null!;
+
+        public Country? Country { get; set; }
+
+        public ICollection<City>? Cities { get; set; }
+
+        public int CitiesNumber => Cities == null ? 0 : Cities.Count;
+    }
+}
+
+
