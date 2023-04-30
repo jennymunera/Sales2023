e163062cdad24ebdd429490fbd1ac3a1456cd3c0
Jenny Munera Serna <jennymunera94@gmail.com>
2023-04-30 00:11:49 -0500

    modify datacontext with categories and add migration and update database

---
+++ Sales.API/Migrations/20230430050742_AddCategories.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sales.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cities_States_StateId",
+                table: "Cities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cities_StateId_Name",
+                table: "Cities");
+
+            migrationBuilder.DropColumn(
+                name: "StatedId",
+                table: "Cities");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StateId",
+                table: "Cities",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_StateId_Name",
+                table: "Cities",
+                columns: new[] { "StateId", "Name" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Name",
+                table: "Categories",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cities_States_StateId",
+                table: "Cities",
+                column: "StateId",
+                principalTable: "States",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cities_States_StateId",
+                table: "Cities");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cities_StateId_Name",
+                table: "Cities");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StateId",
+                table: "Cities",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatedId",
+                table: "Cities",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_StateId_Name",
+                table: "Cities",
+                columns: new[] { "StateId", "Name" },
+                unique: true,
+                filter: "[StateId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cities_States_StateId",
+                table: "Cities",
+                column: "StateId",
+                principalTable: "States",
+                principalColumn: "Id");
+        }
+    }
+}
