6e1353b9693ae49b5dea31146ed4efabdc7b6697
Jenny Munera Serna <jennymunera94@gmail.com>
2023-05-05 13:45:56 -0500

    add filter to coutries, it was modified pagination, countriescontroller and countriesindex

--- Sales.Shared/DTOs/PaginationDTO.cs
+++ Sales.Shared/DTOs/PaginationDTO.cs
@@ -13,6 +13,8 @@ namespace Sales.Shared.DTOs
         public int Page { get; set; } = 1;
 
         public int RecordsNumber { get; set; } = 10;
+
+        public string? Filter { get; set; }
     }
 }
 
