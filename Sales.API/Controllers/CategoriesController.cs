ab840b4c31f67b7449f88c8c97b3e66a7c8f44b5
Jenny Munera Serna <jennymunera94@gmail.com>
2023-05-04 23:59:01 -0500

    modify categories controllers to add pagination

--- Sales.API/Controllers/CategoriesController.cs
+++ Sales.API/Controllers/CategoriesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Sales.API.Data;
+using Sales.API.Helpers;
+using Sales.Shared.DTOs;
 using Sales.Shared.Entities;
 
 namespace Sales.API.Controllers
@@ -18,12 +20,29 @@ namespace Sales.API.Controllers
 
 
         [HttpGet]
-        public async Task<ActionResult> Get()
+        public async Task<ActionResult> Get([FromQuery] PaginationDTO pagination)
         {
-         
-            return Ok(await _context.Categories
+            var queryable = _context.Categories
+                .AsQueryable();
+
+
+            return Ok(await queryable
                 .OrderBy(x => x.Name)
+                .Paginate(pagination)
                 .ToListAsync());
+        }
+
+
+        [HttpGet("totalPages")]
+        public async Task<ActionResult> GetPages([FromQuery] PaginationDTO pagination)
+        {
+            var queryable = _context.Categories
+                .AsQueryable();
+
+
+            double count = await queryable.CountAsync();
+            double totalPages = Math.Ceiling(count / pagination.RecordsNumber);
+            return Ok(totalPages);
         }
 
 
